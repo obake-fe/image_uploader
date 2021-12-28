4fb5a09e9ee43138460c735f576196d47bc41eef
obake-fe <daichismash@gmail.com>
2021-12-28 18:19:46 +0900

    refactor: tsconfig and webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -2,30 +2,23 @@ module.exports = {
 	// モード値を production に設定すると最適化された状態で、
 	// development に設定するとソースマップ有効でJSファイルが出力される
 	mode: "development",
-
-	// メインとなるJavaScriptファイル（エントリーポイント）
 	entry: "./src/main.tsx",
-	// ファイルの出力設定
 	output: {
-		//  出力ファイルのディレクトリ名
-		path: `${__dirname}/dist`,
-		// 出力ファイル名
-		filename: "main.js"
+		path: `${__dirname}/dist`, //  出力ファイルのディレクトリ名
+		filename: "main.js" // 出力ファイル名
 	},
 	module: {
 		rules: [
 			{
-				// 拡張子 .ts もしくは .tsx の場合
-				test: /\.tsx?$/,
-				// TypeScript をコンパイルする
+				test: /\.tsx?$/, // 拡張子 .ts もしくは .tsx の場合
 				use: "ts-loader"
 			}
 		]
 	},
-	// import 文で .ts や .tsx ファイルを解決するため
 	resolve: {
+		// import 文で .ts や .tsx ファイルを解決するため
+		// これを定義しないと import 文で拡張子を書く必要が生まれる。
 		extensions: [".ts", ".tsx", ".js", ".json"]
 	},
-	// ES5(IE11等)向けの指定（webpack 5以上で必要）
-	target: ["web", "es5"],
+	target: ["web", "es5"], // ES5(IE11等)向けの指定（webpack 5以上で必要）
 };
