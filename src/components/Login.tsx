b0f97de53c3223caf73f5b12ff45b5ec020fece6
obake-fe <daichismash@gmail.com>
2022-01-01 20:58:33 +0900

    feat: add login error message

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,21 +1,29 @@
-import React, { VFC } from 'react';
+import React, { useState, VFC } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { signInWithEmailAndPassword } from 'firebase/auth';
 import { auth } from '../firebase/firebase';
 
 export const Login: VFC = () => {
   const navigate = useNavigate();
+  const [errorText, setError] = useState('');
 
   const handleSubmit = async (event: any) => {
     event.preventDefault();
     const { email, password } = event.target.elements;
-    await signInWithEmailAndPassword(auth, email.value, password.value);
-    navigate('/');
+
+    try {
+      await signInWithEmailAndPassword(auth, email.value, password.value);
+      navigate('/');
+    } catch (error: any) {
+      console.log(error);
+      setError(error.message);
+    }
   };
 
   return (
     <div>
       <h1>ログイン</h1>
+      {errorText && <p style={{ color: 'red' }}>{errorText}</p>}
       <form onSubmit={handleSubmit}>
         <div>
           <label htmlFor="email">
