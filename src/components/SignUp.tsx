6bb76495d959ab8f8999cd66f925809b3fc2d35e
obake-fe <daichismash@gmail.com>
2022-01-02 21:03:11 +0900

    feat: add signup redirect

--- src/components/SignUp.tsx
+++ src/components/SignUp.tsx
@@ -1,20 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { useNavigate } from 'react-router-dom';
 import { auth } from '../firebase/firebase';
-import { useAuthContext } from './AuthContext';
 
 export const SignUp = () => {
-  const { user } = useAuthContext();
+  const navigate = useNavigate();
+  const [errorText, setError] = useState('');
   const handleSubmit = async (event: any) => {
     event.preventDefault();
     const { email, password } = event.target.elements;
 
-    await createUserWithEmailAndPassword(auth, email.value, password.value);
+    try {
+      await createUserWithEmailAndPassword(auth, email.value, password.value);
+      navigate('/');
+    } catch (error: any) {
+      setError(error.message);
+    }
   };
 
   return (
     <div>
-      <h1>ユーザ登録 {user ? user.email : ''}</h1>
+      <h1>ユーザ登録</h1>
+      {errorText && <p style={{ color: 'red' }}>{errorText}</p>}
       <form onSubmit={handleSubmit}>
         <div>
           <label htmlFor="email">
